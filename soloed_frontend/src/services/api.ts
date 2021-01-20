82189195d36f1878c6a04c7f6786855c3a2cece7
Felipe Monteiro <felipedeamonteiro@gmail.com>
2021-01-20 18:14:51 -0300

    Change baseURL to use proper API

--- soloed_frontend/src/services/api.ts
+++ soloed_frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Connection made with the api
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://localhost:3334',
 });
 
 export default api;

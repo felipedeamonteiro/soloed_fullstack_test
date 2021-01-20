1153cb16f6e05c2c26fb9d0b24b725e28dd397f8
Felipe Monteiro <felipedeamonteiro@gmail.com>
2021-01-20 01:13:59 -0300

    Finish CartMenu and styles

--- soloed_frontend/src/components/Header/index.tsx
+++ soloed_frontend/src/components/Header/index.tsx
@@ -2,21 +2,19 @@ import React, { useState, useCallback } from 'react';
 
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { FaShoppingCart } from 'react-icons/fa';
+import { GrFormAdd } from 'react-icons/gr';
+import { IoMdRemove } from 'react-icons/io';
 import { Container } from './styles';
 import Logo from '../../assets/logo_soloed.svg';
+import AppleWatch from '../../assets/apple-watch.jpg';
+import JBLSpeaker from '../../assets/JBL_speaker.jpg';
 
 const Header: React.FC = () => {
   const [sidebar, setSidebar] = useState<boolean>(false);
 
-  const handleOpenMenu = useCallback(() => {
-    console.log('Abrindo o menu de carrinho');
-    setSidebar(true);
-  }, []);
-
-  const handleCloseMenu = useCallback(() => {
-    console.log('Fechando o menu de carrinho');
-    setSidebar(false);
-  }, []);
+  const handleCartBar = useCallback(() => {
+    setSidebar(!sidebar);
+  }, [sidebar]);
 
   return (
     <Container>
@@ -26,27 +24,85 @@ const Header: React.FC = () => {
         </div>
 
         <div className="cart-div">
-          <button type="button" onClick={handleOpenMenu}>
+          <button type="button" onClick={handleCartBar}>
             <FaShoppingCart size={20} />
             <strong>5</strong>
           </button>
         </div>
       </div>
 
-      <nav className={sidebar ? 'nav-menu-active' : 'nav-menu'}>
-        <ul className="nav-menu-items">
-          <li className="navbar-toggle">
-            <button type="button" onClick={handleCloseMenu}>
-              <AiFillCloseCircle size={20} />
-            </button>
-          </li>
-          <li>
-            <div className="cart-items">Item do Carrinho 1</div>
-          </li>
-          <li>
-            <div className="cart-items">Item do Carrinho 2</div>
-          </li>
-        </ul>
+      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+        <div className="top-div">
+          <h2>Carrinho de compras</h2>
+          <AiFillCloseCircle
+            size={38}
+            onClick={handleCartBar}
+            style={{ cursor: 'pointer' }}
+          />
+        </div>
+        <div className="items-div">
+          <ul className="nav-menu-items">
+            <li>
+              <img src={AppleWatch} alt="Apple Watch" />
+              <p>Apple Watch Series 4 GPD</p>
+              <div className="add-remove">
+                <p>Qtd:</p>
+                <div className="qtd-div">
+                  <button type="button">
+                    <IoMdRemove
+                      size={11}
+                      color="#000"
+                      style={{ cursor: 'pointer' }}
+                    />
+                  </button>
+                  <input type="number" readOnly value="3" />
+                  <button type="button">
+                    <GrFormAdd
+                      size={11}
+                      color="#000"
+                      style={{ cursor: 'pointer' }}
+                    />
+                  </button>
+                </div>
+              </div>
+              <strong>R$ 399,00</strong>
+              <AiFillCloseCircle size={18} style={{ cursor: 'pointer' }} />
+            </li>
+            <li>
+              <img src={JBLSpeaker} alt="JBL Speaker" />
+              <p>JBL Speaker</p>
+              <div className="add-remove">
+                <p>Qtd:</p>
+                <div className="qtd-div">
+                  <button type="button">
+                    <IoMdRemove
+                      size={11}
+                      color="#000"
+                      style={{ cursor: 'pointer' }}
+                    />
+                  </button>
+                  <input type="number" readOnly value="1" />
+                  <button type="button">
+                    <GrFormAdd
+                      size={11}
+                      color="#000"
+                      style={{ cursor: 'pointer' }}
+                    />
+                  </button>
+                </div>
+              </div>
+              <strong>R$ 299,00</strong>
+              <AiFillCloseCircle size={18} style={{ cursor: 'pointer' }} />
+            </li>
+          </ul>
+        </div>
+        <div className="bottom-div">
+          <div className="total-div">
+            <h2>Total:</h2>
+            <h2>R$ 798,00</h2>
+          </div>
+          <button type="button">Finalizar Compra</button>
+        </div>
       </nav>
     </Container>
   );
--- soloed_frontend/src/components/Header/styles.ts
+++ soloed_frontend/src/components/Header/styles.ts
@@ -7,6 +7,7 @@ export const Container = styled.div`
 
   .nav-bar {
     background: #ffff00;
+    height: 100px;
 
     display: flex;
     align-items: center;
@@ -50,14 +51,142 @@ export const Container = styled.div`
     width: 486px;
     height: 100vh;
     display: flex;
-    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
     position: fixed;
-    /* right: -300px; */
-    right: 0;
+    top: 0;
+    right: -100%;
     transition: 850ms;
+    box-shadow: -5px 0px 6px rgba(0, 0, 0, 0.13);
+
+    .top-div {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      flex-direction: row;
+      margin-top: 36px;
+
+      h2 {
+        margin-left: 47px;
+        max-width: 180px;
+      }
+
+      svg {
+        margin-right: 22px;
+      }
+    }
+
+    .items-div {
+      margin-top: 10px;
+      height: 60%;
+      display: flex;
+      justify-content: center;
+      align-items: flex-start;
+
+      li {
+        list-style: none;
+        background: #fff;
+        box-shadow: -2px 2px 10px rgba(0, 0, 0, 0.05);
+        border-radius: 8px;
+        width: 380px;
+        height: 95px;
+
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-direction: row;
+
+        & + li {
+          margin-top: 28px;
+        }
+
+        img {
+          height: 60px;
+        }
+
+        p {
+          width: 113px;
+          font-size: 13px;
+        }
+
+        .add-remove {
+          display: flex;
+          flex-direction: column;
+
+          p {
+            font-size: 9px;
+          }
+
+          .qtd-div {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            width: 66px;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+
+            input {
+              padding-left: 12px;
+              border: 0;
+              color: #666;
+              width: 35px;
+              font-size: 11px;
+            }
+
+            button {
+              border: 0;
+              border-right: 1px solid #ddd;
+              background: none;
+            }
+
+            input + button {
+              border-left: 1px solid #ddd;
+              border-right: 0;
+            }
+          }
+        }
+
+        strong {
+          font-size: 14px;
+        }
+
+        strong + svg {
+          margin: 0 0 87px -18px;
+        }
+      }
+    }
+
+    .bottom-div {
+      .total-div {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 42px;
+
+        > h2 {
+          margin-left: 47px;
+
+          & + h2 {
+            margin-right: 47px;
+          }
+        }
+      }
+
+      button {
+        width: 100%;
+        height: 100px;
+        background-color: #000000;
+        color: #ffff00;
+        text-align: center;
+        border: 0;
+        font-size: 28px;
+        font-weight: 700;
+      }
+    }
   }
 
-  .nav-menu-active {
+  .nav-menu.active {
     right: 0;
     transition: 350ms;
   }

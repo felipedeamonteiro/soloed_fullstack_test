fd3ef3977ec8efb57bb479946bee0435c545ade2
Felipe Monteiro <felipedeamonteiro@gmail.com>
2021-07-31 01:13:39 -0300

    Fix eslint config

--- soloed_frontend/src/services/api.ts
+++ soloed_frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Connection made with the api
 const api = axios.create({
-  baseURL: 'http://localhost:3334',
+  baseURL: 'http://localhost:3333',
 });
 
 export default api;
